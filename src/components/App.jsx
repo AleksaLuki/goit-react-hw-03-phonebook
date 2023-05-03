fda3496d762065a27176307b71e7cf7cf0985ed2
AleksaLuki <dimirtiy9000@gmail.com>
2023-05-03 18:53:56 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ContactsForm } from './ContactsForm/ContactsForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+import React, { Component } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contactsFromStorage = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contactsFromStorage) {
+      this.setState({ contacts: contactsFromStorage });
+
+      if (contactsFromStorage.length === 0) {
+        toast.info('No contacts in your list yet');
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+
+  handleAddContact = contact => {
+    if(this.state.contacts.some((item) => item.name === contact.name)) {
+      toast.error("Contact already exists")
+      return true 
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+    return false;
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  handleChangeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  habdleFilterContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase().trim())
+    );
+  };
+  render() {
+    return (
+      <>
+        <ContactsForm addContact={this.handleAddContact} />
+        <Filter
+          value={this.state.filter}
+          handleChange={this.handleChangeFilter}
+        />
+        <ContactsList
+          contacts={this.habdleFilterContacts()}
+          deleteContact={this.handleDeleteContact}
+        />
+        <Toaster/>
+      </>
+    );
+  }
+}
